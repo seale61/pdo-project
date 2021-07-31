079474be056fd4c755234763dd3ba0a2315e5651
Charles Seale <seale61@gmail.com>
2021-07-30 22:24:53 -0400

    More spacing adjustmen, nothing to lose sleep over

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 <?php
-    require_once('./vendor/autoload.php');
-    require_once('./classes/pdo.class.php');
 
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-    $dotenv->load();
+require_once('./vendor/autoload.php');
+require_once('./classes/pdo.class.php');
 
-    $db = new pdoData();
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
-    if($db->connect()) {
-        echo "Hot diggity damn!\n";
-        $db->close();
-    }
-    
-    if($db->error()) {
-        echo $db->error();
-    }
+$db = new pdoData();
 
+if($db->connect()) {
+    echo "Hot diggity damn!\n";
+    $db->close();
+}
+
+if($db->error()) {
+    echo $db->error();
+}
