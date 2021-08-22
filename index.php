5a90f82073eb9f11eaf28242f63a5e94e494ed22
Charles Seale <seale61@gmail.com>
2021-08-22 11:24:02 -0400

    This is a test page and it's contents constantly changes - pay no mind

--- index.php
+++ index.php
@@ -7,12 +7,30 @@ $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
 $db = new pdoData();
+$output = [];
 
-if($db->connect()) {
-    echo "Hot diggity damn!\n";
-    $db->close();
-}
+$sql = "SELECT * FROM people";
+$binds = [];
 
-if($db->error()) {
-    echo $db->error();
-}
+$fetchMode = '';
+$data = $db->query($sql, $binds, $fetchMode);
+array_push($output, 'All persons');
+array_push($output, $data);
+
+$sql = "SELECT * FROM people WHERE last_name = ?";
+$binds = ['Seale'];
+
+$fetchMode = '';
+$data = $db->query($sql, $binds, $fetchMode);
+array_push($output, 'Last name is '.$binds[0]);
+array_push($output, $data);
+
+$sql = "SELECT * FROM people WHERE last_name <> ?";
+$binds = ['Seale'];
+
+$fetchMode = '';
+$data = $db->query($sql, $binds, $fetchMode);
+array_push($output, 'Last name is not '.$binds[0]);
+array_push($output, $data);
+
+echo json_encode($output);
